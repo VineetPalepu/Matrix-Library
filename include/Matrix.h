5896d8b681a99a6c88e44e3e2686b373659cbcf9
Vineet Palepu <vineetmpalepu@gmail.com>
2020-08-19 20:00:09 -0700

    removed all OpenCL code

--- include/Matrix.h
+++ include/Matrix.h
@@ -1,5 +1,4 @@
 #pragma once
-#define CL_HPP_TARGET_OPENCL_VERSION 220
 #include <tuple>
 #include <iostream>
 #include <iomanip>
@@ -9,16 +8,7 @@
 #include <typeinfo>
 #include <streambuf>
 #include <regex>
-#include <CL/cl2.hpp>
 
-#define DEVICE_GPU 0
-#define DEVICE_CPU 1
-
-// TODO: Get CL to not crash during long matrix multiplications
-// figure out why CL crashes on large matrix multiplications
-// Use multiple kernels to prevent long running kernel from
-// stopping execution. Graphics drivers stop a process if 
-// it hangs for a long time
 // TODO: Add resize method? or at least figure out whether or not
 // a feature is necessary. Assignment has the same functionality, 
 // but a resize() method could be more intuitive.
@@ -35,82 +25,6 @@ namespace MatrixLibrary
 		int m_size;
 		T *m_data;
 		const bool m_rowMajor;
-
-#pragma region OpenCL Static Initialization Code
-		class OpenCL
-		{
-			friend class Matrix<T>;
-		  private:
-			static inline cl_context context;
-			static inline cl_command_queue queue;
-			static inline cl_device_id device;
-			static inline const int DEVICE = DEVICE_CPU;
-			static inline bool first = true;
-			static inline std::string code;
-			
-			// TODO: Instead of using a DEVICE_GPU or DEVICE_CPU constant, change the clGetPlatformInfo() call
-			// to query CL_DEVICE_TYPE_CPU or CL_DEVICE_TYPE_GPU instead
-			static int clSetup()
-			{
-				if (!first)
-					return 0;
-
-				first = false;
-
-				//std::cout << "Listing Platform Devices" << std::endl;
-				cl_platform_id platform;
-				clGetPlatformIDs(1, &platform, nullptr);
-				char name[64];
-				clGetPlatformInfo(platform, CL_PLATFORM_NAME, 64 * sizeof(char), name, NULL);
-				//std::cout << "Platform:" << name << std::endl;
-				cl_uint numDevices;
-				clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, 0, NULL, &numDevices);
-				if (numDevices == 0)
-				{
-					std::cout << "No valid OpenCL device found" << std::endl; 
-				}
-				cl_device_id *devices = new cl_device_id[numDevices];
-				clGetDeviceIDs(platform, CL_DEVICE_TYPE_ALL, numDevices, devices, NULL);
-				for (int i = 0; i < numDevices; i++)
-				{
-					clGetDeviceInfo(devices[i], CL_DEVICE_NAME, 64 * sizeof(char), name, NULL);
-					//std::cout << "\tDevice " << i << ": " << name << std::endl;
-				}
-
-				//std::cout << "Creating Context" << std::endl;
-				cl_int err;
-				context = clCreateContext(NULL, numDevices, devices, NULL, NULL, &err);
-				if (err != 0)
-				{
-					std::cout << "Error creating context: " << err << std::endl;
-					delete[] devices;
-					return -1;
-				}
-
-				device = devices[DEVICE];
-
-				//std::cout << "Creating Command Queue" << std::endl;
-				queue = clCreateCommandQueueWithProperties(context, device, NULL, &err);
-				if (err != 0)
-				{
-					std::cout << "Error creating queue: " << err << std::endl;
-					delete[] devices;
-					return -1;
-				}
-
-				std::fstream inputFile;
-				size_t length;
-				inputFile.open("mat_mul_kernel.cl", std::ios::in | std::ios_base::binary);
-				inputFile.seekg(0, std::ios::end);
-				length = inputFile.tellg();
-				inputFile.seekg(0, std::ios::beg);
-				code = std::string((std::istreambuf_iterator<char>(inputFile)), std::istreambuf_iterator<char>());
-
-				delete[] devices;
-				return 0;
-			}
-		};
-#pragma endregion
 
 #pragma region Random
 		template <class NumericType, typename Enable = void>
@@ -154,213 +68,6 @@ namespace MatrixLibrary
 		};
 #pragma endregion
 
-#pragma region Exceptions
-		class INDEX_OUT_OF_BOUNDS{};
-		class MATRIX_SHAPE_ERROR{};
-		class NO_OPENCL_IMPLEMENTATION_FOUND{};
-		class NO_OPENCL_DEVICE_FOUND{};
-		class OPENCL_ERROR{};
-#pragma endregion
-
-#pragma region OpenCL Initializer
-		struct Initializer
-		{
-			Initializer()
-			{
-				if (OpenCL::clSetup() == 0 && Matrix::createProgram() == 0)
-					supportsCL = true;
-			}
-		};
-
-		static inline Initializer init = Initializer();
-		static inline cl_program program;
-		static inline cl_kernel mat_mul_kernel;
-		static inline bool supportsCL = false;
-
-		static int createProgram()
-		{
-			if (typeid(T) == typeid(char) ||
-				typeid(T) == typeid(unsigned char) ||
-				typeid(T) == typeid(short) ||
-				typeid(T) == typeid(unsigned short) ||
-				typeid(T) == typeid(int) || 
-				typeid(T) == typeid(unsigned int) ||
-				typeid(T) == typeid(long) || 
-				typeid(T) == typeid(unsigned long) || 
-				typeid(T) == typeid(float) || 
-				typeid(T) == typeid(double))
-			{
-				supportsCL = true;
-			}
-			else
-			{
-				supportsCL = false;
-				return -1;
-			}
-			//std::cout << "Creating Program" << std::endl;
-			cl_int err;
-
-			OpenCL::code = std::regex_replace(OpenCL::code, std::regex("T"), typeid(T).name());
-			const char* src = OpenCL::code.c_str();
-
-			program = clCreateProgramWithSource(OpenCL::context, 1, &src, NULL, &err);
-			if (err != 0)
-			{
-				std::cout << "Error creating program: " << err << std::endl;
-				return -1;
-			}
-
-			//std::cout << "Building Program" << std::endl;
-			err = clBuildProgram(program, 1, &OpenCL::device, NULL, NULL, NULL);
-			if (err != 0)
-			{
-				std::cout << "Error building program: " << err << std::endl;
-				if (err == -11)
-				{
-					size_t log_size;
-					clGetProgramBuildInfo(program, OpenCL::device, CL_PROGRAM_BUILD_LOG, NULL, NULL, &log_size);
-					char* log = new char[log_size];
-					clGetProgramBuildInfo(program, OpenCL::device, CL_PROGRAM_BUILD_LOG, log_size, log, NULL);
-					std::cout << std::endl << log << std::endl;
-					delete[] log;
-				}
-				return -1;
-			}
-
-			//std::cout << "Creating Kernel" << std::endl;
-			mat_mul_kernel = clCreateKernel(program, "matMul", &err);
-			if (err != 0)
-			{
-				std::cout << "Error creating kernel: " << err << std::endl;
-				return -1;
-			}
-		}
-
-		static Matrix clMul(const Matrix &mat1, const Matrix &mat2)
-		{
-			Matrix result{mat1.rows(), mat2.columns()};
-			//cout << "Creating Buffers" << endl;
-			cl_int err;
-			cl_mem mat1Buf = clCreateBuffer(OpenCL::context, CL_MEM_READ_ONLY, sizeof(T) * mat1.size(), NULL, &err);
-			if (err != 0)
-			{
-				std::cout << "Error creating mat1Buf: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-			cl_mem mat2Buf = clCreateBuffer(OpenCL::context, CL_MEM_READ_ONLY, sizeof(T) * mat2.size(), NULL, &err);
-			if (err != 0)
-			{
-				std::cout << "Error creating mat2Buf: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-			cl_mem resultBuf = clCreateBuffer(OpenCL::context, CL_MEM_WRITE_ONLY, sizeof(T) * result.size(), NULL, &err);
-			if (err != 0)
-			{
-				std::cout << "Error creating resultBuf: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-
-			//cout << "Copying Data to Buffers" << endl;
-			err = clEnqueueWriteBuffer(OpenCL::queue, mat1Buf, CL_TRUE, 0, mat1.size() * sizeof(T), mat1.m_data, 0, NULL, NULL);
-			if (err != 0)
-			{
-				std::cout << "Error writing to mat1Buf: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-			err = clEnqueueWriteBuffer(OpenCL::queue, mat2Buf, CL_TRUE, 0, mat2.size() * sizeof(T), mat2.m_data, 0, NULL, NULL);
-			if (err != 0)
-			{
-				std::cout << "Error writing to mat2Buf: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-			int m1_rows = mat1.rows();
-			int m1_columns = mat1.columns();
-			int m2_columns = mat2.columns();
-			//cout << "Setting Kernel Arguments" << endl;
-			err = clSetKernelArg(mat_mul_kernel, 0, sizeof(mat1Buf), &mat1Buf);
-			if (err != 0)
-			{
-				std::cout << "Error setting arg 0: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-			err = clSetKernelArg(mat_mul_kernel, 1, sizeof(mat2Buf), &mat2Buf);
-			if (err != 0)
-			{
-				std::cout << "Error setting arg 1: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-			err = clSetKernelArg(mat_mul_kernel, 2, sizeof(int), &m1_rows);
-			if (err != 0)
-			{
-				std::cout << "Error setting arg 2: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-			err = clSetKernelArg(mat_mul_kernel, 3, sizeof(int), &m1_columns);
-			if (err != 0)
-			{
-				std::cout << "Error setting arg 3: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-			err = clSetKernelArg(mat_mul_kernel, 4, sizeof(int), &m2_columns);
-			if (err != 0)
-			{
-				std::cout << "Error setting arg 4: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-			err = clSetKernelArg(mat_mul_kernel, 5, sizeof(resultBuf), &resultBuf);
-			if (err != 0)
-			{
-				std::cout << "Error setting arg 5: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-
-			//cout << "Enqueueing Kernel" << endl;
-
-			const int DIMS = 2;
-			size_t *g_work_size = new size_t[DIMS];
-			g_work_size[0] = ((mat1.rows() / 16) + 1) * 16;
-			g_work_size[1] = ((mat2.columns() / 16) + 1) * 16;
-			size_t *l_work_size = new size_t[DIMS];
-			l_work_size[0] = 16;
-			l_work_size[1] = 16;
-			err = clEnqueueNDRangeKernel(OpenCL::queue, mat_mul_kernel, DIMS, NULL, g_work_size, l_work_size, 0, NULL, NULL);
-			if (err != 0)
-			{
-				std::cout << "Error enqueueing kernel: " << err << std::endl;
-				delete[] g_work_size;
-				delete[] l_work_size;
-
-				throw OPENCL_ERROR();
-			}
-			delete[] g_work_size;
-			delete[] l_work_size;
-
-			//cout << "Copying Buffer to Host" << endl;
-			err = clEnqueueReadBuffer(OpenCL::queue, resultBuf, CL_TRUE, 0, sizeof(T) * result.size(), result.m_data, 0, NULL, NULL);
-			if (err != 0)
-			{
-				std::cout << "Error reading resultBuf: " << err << std::endl;
-				throw OPENCL_ERROR();
-			}
-
-			clReleaseMemObject(mat1Buf);
-			clReleaseMemObject(mat2Buf);
-			clReleaseMemObject(resultBuf);
-
-			return result;
-		}
-
-		static void clFlushAll() // call at end of program (automatically?)
-		{
-			clFlush(OpenCL::queue);
-			clFinish(OpenCL::queue);
-			clReleaseKernel(mat_mul_kernel);
-			clReleaseProgram(program);
-			clReleaseCommandQueue(OpenCL::queue);
-			clReleaseContext(OpenCL::context);
-		}
-#pragma endregion
-
 	  public:
 
 #pragma region Constructors / Assignment
@@ -371,7 +78,7 @@ namespace MatrixLibrary
 			if (r < 0 || c < 0)
 			{
 				std::cerr << "Cannot construct a matrix of size " << shape() << std::endl;
-				throw MATRIX_SHAPE_ERROR();
+				throw std::exception();
 			}
 		}
 
@@ -682,11 +389,11 @@ namespace MatrixLibrary
 		{
 			if (rowStart > rowEnd || colStart > colEnd)
 			{
-				throw MATRIX_SHAPE_ERROR();
+				throw std::exception();
 			}
 			if (rowStart < 0 || colStart < 0 || rowEnd > m_rows || colEnd > m_columns)
 			{
-				throw INDEX_OUT_OF_BOUNDS();
+				throw std::exception();
 			}
 
 			Matrix result{ rowEnd - rowStart, colEnd - colStart };
@@ -715,7 +422,7 @@ namespace MatrixLibrary
 			if (m_rows != mat.m_rows)
 			{
 				std::cerr << "The number of rows does not match: " << shape() << ", " << mat.shape() << std::endl;
-				throw MATRIX_SHAPE_ERROR();
+				throw std::exception();
 			}
 
 			Matrix result{ m_rows, m_columns + mat.m_columns };
@@ -738,7 +445,7 @@ namespace MatrixLibrary
 			if (splitColumn > m_columns)
 			{
 				std::cerr << "The supplied index, " << splitColumn << ", must be less than or equal to the number of columns, " << m_columns << std::endl;
-				throw INDEX_OUT_OF_BOUNDS();
+				throw std::exception();
 			}
 
 			Matrix m1{ m_rows, splitColumn };
@@ -767,7 +474,7 @@ namespace MatrixLibrary
 			if (m_columns != mat.m_columns)
 			{
 				std::cerr << "The number of columns does not match: " << shape() << ", " << mat.shape() << std::endl;
-				throw MATRIX_SHAPE_ERROR();
+				throw std::exception();
 			}
 
 			Matrix result{ m_rows + mat.m_rows, m_columns };
@@ -790,7 +497,7 @@ namespace MatrixLibrary
 			if (splitRow > m_columns)
 			{
 				std::cerr << "The supplied index, " << splitRow << ", must be less than or equal to the number of rows, " << m_rows << std::endl;
-				throw INDEX_OUT_OF_BOUNDS();
+				throw std::exception();
 			}
 
 			Matrix m1{ splitRow, m_columns };
@@ -821,7 +528,7 @@ namespace MatrixLibrary
 				if (m_size % c != 0)
 				{
 					std::cerr << "Cannot reshape matrix with shape " << shape() << " to have " << c << " columns" << std::endl;
-					throw MATRIX_SHAPE_ERROR();
+					throw std::exception();
 				}
 
 				m_rows = m_size / c;
@@ -832,7 +539,7 @@ namespace MatrixLibrary
 				if (m_size % r != 0)
 				{
 					std::cerr << "Cannot reshape matrix with shape " << shape() << " to have " << r << " rows" << std::endl;
-					throw MATRIX_SHAPE_ERROR();
+					throw std::exception();
 				}
 
 				m_rows = r;
@@ -843,7 +550,7 @@ namespace MatrixLibrary
 				if (m_size != r * c)
 				{
 					std::cerr << "The size of the matrix, " << m_size << "does not match the size given by r * c, " << r * c << std::endl;
-					throw MATRIX_SHAPE_ERROR();
+					throw std::exception();
 				}
 
 				m_rows = r;
@@ -873,7 +580,7 @@ namespace MatrixLibrary
 			if (!isSquare())
 			{
 				std::cerr << "matrix of shape " << shape() << " is not square" << std::endl;
-				throw MATRIX_SHAPE_ERROR();
+				throw std::exception();
 			}
 
 			Matrix augmented = hStack(identity(m_rows)).rowReduce();
@@ -932,7 +639,7 @@ namespace MatrixLibrary
 		const T& operator[](int index) const
 		{
 			if (index < 0 || index >= m_size)
-				throw INDEX_OUT_OF_BOUNDS();
+				throw std::exception();
 
 			return m_data[index];
 		}
@@ -964,7 +671,7 @@ namespace MatrixLibrary
 		friend Matrix operator+(const Matrix& mat1, const Matrix& mat2)
 		{
 			if (mat1.m_rows != mat2.m_rows || mat1.m_columns != mat2.m_columns)
-				throw MATRIX_SHAPE_ERROR();
+				throw std::exception();
 
 			Matrix result{ mat1.m_rows,mat1.m_columns };
 			for (int i = 0; i < result.m_rows; i++)
@@ -1016,7 +723,7 @@ namespace MatrixLibrary
 		friend Matrix operator-(const Matrix& mat1, const Matrix& mat2)
 		{
 			if (mat1.m_rows != mat2.m_rows || mat1.m_columns != mat2.m_columns)
-				throw MATRIX_SHAPE_ERROR();
+				throw std::exception();
 
 			Matrix result{ mat1.m_rows, mat1.m_columns };
 			for (int i = 0; i < result.m_rows; i++)
@@ -1079,29 +786,23 @@ namespace MatrixLibrary
 			if (mat1.m_columns != mat2.m_rows)
 			{
 				std::cerr << "attempted to multiply matrices of sizes " << mat1.shape() << ", " << mat2.shape() << std::endl;
-				throw MATRIX_SHAPE_ERROR();
+				throw std::exception();
 			}
 
-			// TODO: implement clMul for matrices stored in column major order
-			if (supportsCL && mat1.isRowMajor() && mat2.isRowMajor())
-				return clMul(mat1, mat2);
-			else
+			Matrix result{ mat1.m_rows, mat2.m_columns };
+			for (int i = 0; i < result.m_rows; i++)
 			{
-				Matrix result{ mat1.m_rows, mat2.m_columns };
-				for (int i = 0; i < result.m_rows; i++)
+				for (int j = 0; j < result.m_columns; j++)
 				{
-					for (int j = 0; j < result.m_columns; j++)
+					T sum = 0;
+					for (int k = 0; k < mat1.m_columns; k++)
 					{
-						T sum = 0;
-						for (int k = 0; k < mat1.m_columns; k++)
-						{
-							sum += mat1(i, k) * mat2(k, j);
-						}
-						result(i, j) = sum;
+						sum += mat1(i, k) * mat2(k, j);
 					}
+					result(i, j) = sum;
 				}
-				return result;
 			}
+			return result;
 		}
 
 		friend Matrix operator*(const Matrix& mat, T scl)
@@ -1135,7 +836,7 @@ namespace MatrixLibrary
 		Matrix scalarMultiply(const Matrix& mat) const
 		{
 			if (m_rows != mat.m_rows || m_columns != mat.m_columns)
-				throw MATRIX_SHAPE_ERROR();
+				throw std::exception();
 
 			Matrix result{ m_rows, m_columns };
 			for (int i = 0; i < mat.m_rows; i++)
