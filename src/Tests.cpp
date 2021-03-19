4b9b854fe8fa5763e179cfd836da3f9c967c8942
Vineet Palepu <vineetmpalepu@gmail.com>
2021-03-18 22:44:04 -0700

    Removed graphics specific tests

--- src/Tests.cpp
+++ src/Tests.cpp
@@ -7,11 +7,6 @@ using namespace std;
 
 int main()
 {
-	Vec4 v{1, 324, -12, 1};
-	cout << v[0] << " " << v[1] << " " << v[2] << " " << v[3] << endl;
-	Mat4 t = Mat4::identity();
-	t = t.translate(6, 3, -3);
-	v = t * v;
-	cout << v[0] << " " << v[1] << " " << v[2] << " " << v[3] << endl;
+	
 }
 	
